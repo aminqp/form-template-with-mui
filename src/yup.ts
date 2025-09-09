fd328bcefc72fd33e2cd73d67b0bf6fdbb842f72
amin motamedi <aminqp@gmail.com>
2025-09-09 15:53:57 +0000

    use contants in Yup schema

--- src/yup.ts
+++ src/yup.ts
@@ -1,13 +1,18 @@
 import * as Yup from "yup";
+import { INPUT_NAMES, AGE_OPTIONS, GENDER_OPTIONS } from "./constants";
 
 export const schema = Yup.object().shape({
-  age: Yup.string().required(),
-  email: Yup.string()
+  [INPUT_NAMES.AGE]: Yup.mixed().oneOf(AGE_OPTIONS.map((opt) => opt.value)),
+  [INPUT_NAMES.EMAIL]: Yup.string()
     .required()
     .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
-  gender: Yup.string().required(),
-  password: Yup.string()
+  [INPUT_NAMES.GENDER]: Yup.mixed().oneOf(
+    GENDER_OPTIONS.map((opt) => opt.value)
+  ),
+  [INPUT_NAMES.PASSWORD]: Yup.string()
     .required()
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/),
-  terms: Yup.boolean().oneOf([true], "terms is a required field").required(),
+  [INPUT_NAMES.TERMS]: Yup.boolean()
+    .oneOf([true], "terms is a required field")
+    .required(),
 });
